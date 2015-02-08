c83d9f52f24069b7925dcee3cd4412bfc8843171
Rohit Nawani <rohitnwn@gmail.com>
2015-02-08 22:02:28 +0530

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image, styles: { medium: "300*300", thumb: "100*100>" }
+  validates_attachment_content_type :image, :content_type => ["imgae/jpeg", "image/jpg", "image/png" ]
 
   def tag_list
     tags.join(", ")
